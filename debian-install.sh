83e7b017413bdedde40265f0d82f13ccd57d7cc1
LfPi2 <117949259+LfPi2@users.noreply.github.com>
2022-11-14 14:26:51 +0000

    Added debian-install.sh for debian base install

---
+++ debian-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ $(id -u) = "0" ]; then
+	echo "You should not run the script as root"
+	exit
+fi
+
+cd ~
+
+sudo apt-get install git gcc make man-db
+sudo apt-get install xorg pulseaudio
+sudo apt-get install libx11-dev libxft-dev libxinerama-dev
+sudo apt-get install neofetch firefox-esr thunar neovim pavucontrol xdg-user-dirs
+
+xdg-user-dirs-update
+
+mkdir ~/repos
+cd ~/repos
+
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+
+cd dwm
+make
+sudo make clean install
+cd ../dmenu
+make
+sudo make clean install
+
+cd ~
+
+echo "setxkbmap pt" >> .xinitrc
+echo "feh --bg-scale /home/$USER/Pictures/wallpaper.png" >> .xinitrc
+echo "exec dwm" >> .xinitrc
+
+echo "startx" >> .bash_profile
+
+sudo shutdown -h now
